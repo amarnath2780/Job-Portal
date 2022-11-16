fff3a5d3a8c98b3670143619bed28ede75fc4634
Amarnath <motorolaonefusion@gmail.com>
2022-11-16 23:20:27 +0530

    Company Added

--- backend/job_portal/api/urls.py
+++ backend/job_portal/api/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path,include
-from recruiter.views import ApplicationView
+from recruiter.views import ApplicationView , CompanyAddView , CompanyView
+from rest_framework import routers
 
+
+router = routers.DefaultRouter()
+router.register(r'view-company', CompanyView , basename="view-company")
 
 urlpatterns = [
     path('user/', include('accounts.urls')),
-    path('app/' , ApplicationView.as_view() , name="application")
+    path('app/' , ApplicationView.as_view() , name="application"),
+    path('add-company/', CompanyAddView.as_view() , name="company_add"),
 ]
+
+urlpatterns += router.urls
--- backend/job_portal/recruiter/views.py
+++ backend/job_portal/recruiter/views.py
@@ -3,18 +3,32 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from accounts.models import Account
 from .models import Company, RecruiterProfile, Application
-from .serializers import CompanySerializer, RecruiterProfileSerializer, ApplicationSerializer
+from .serializers import CompanySerializer, RecruiterProfileSerializer, ApplicationSerializer 
 from rest_framework.decorators import api_view,permission_classes
 from rest_framework import status
 from django.http import JsonResponse
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
+
 # Create your views here.
 
 
 class CompanyView(ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+
+
+class CompanyAddView(APIView):
+    def post(self, request):
+        serilizer = CompanySerializer(data = request.data)
+        if serilizer.is_valid():
+            serilizer.save()
+            return Response({'message' : "company add successfully"} , status=status.HTTP_200_OK)
+        else:
+            print('serilzer is valid')
+            print(serilizer.errors)
+            return Response({'message' : 'Details is not valid'} , status=status.HTTP_400_BAD_REQUEST)
+
 
 
 class RecruiterProfileView(ModelViewSet):
@@ -27,10 +41,10 @@ class ApplicationView(APIView):
         serilizer = ApplicationSerializer(data=request.data)
         if serilizer.is_valid():
             serilizer.save()
-            return Response({'message' : "created successfullu" } , status=status.HTTP_200_OK)
+            return Response({'message' : "created successfully" } , status=status.HTTP_200_OK)
         else:
             print('serilzer not valid')
-            return Response({'message' : 'not created'} , status=status.HTTP_400_BAD_REQUEST )
+            return Response({'message' : 'Details are not  Valid'} , status=status.HTTP_400_BAD_REQUEST )
 
 
 
