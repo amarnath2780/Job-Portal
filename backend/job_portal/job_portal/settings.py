c57d4dc351a75ad6165e94bfbdd360c762e00168
Amarnath <motorolaonefusion@gmail.com>
2022-12-16 17:56:53 +0530

    dataBase Updated

--- backend/job_portal/job_portal/settings.py
+++ backend/job_portal/job_portal/settings.py
@@ -102,23 +102,23 @@ CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# DATABASES={
-#    'default':{
-#       'ENGINE':'django.db.backends.postgresql_psycopg2',
-#       'NAME':'myproject',
-#       'USER':'myprojectuser',
-#       'PASSWORD':'kali2780',
-#       'HOST':'localhost',
-#       'PORT':'',
-#    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES={
+   'default':{
+      'ENGINE':'django.db.backends.postgresql_psycopg2',
+      'NAME':'myproject',
+      'USER':'myprojectuser',
+      'PASSWORD':'kali2780',
+      'HOST':'localhost',
+      'PORT':'',
+   }
+}
 
 # DATABASES={
 #    'default':{
